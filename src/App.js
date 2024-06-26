1a9b03c2fa9985d476f03f81ca1492e57eaf0464
bokyung626 <ruddl033@naver.com>
2024-06-26 23:28:43 +0900

    feat: Initialized project with CRA

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Card } from "./components/Card/Card";
+import { Search } from "./components/Search/Search";
+import { InputDouble } from "./components/InputDouble/InputDouble";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>1</h1>
+      <Search />
+      <h1>2</h1>
+      <InputDouble />
+      <h1>3</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
