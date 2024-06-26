5ca2f7fb2df0a74ed2308810fbae34bf15daeb4d
bokyung626 <ruddl033@naver.com>
2024-06-27 04:17:14 +0900

    .

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -17,8 +17,16 @@ export const Card = () => {
           onClickHandler();
         }}
       >
-        <img className="front" src="/images/cherry.jpg" />
-        <img className="back" src="/images/flower.jpg" />
+        <img
+          className="front"
+          src="https://i.pinimg.com/564x/25/db/84/25db84915447daf226a14561a3530ff7.jpg"
+          alt="front"
+        />
+        <img
+          className="back"
+          src="https://i.pinimg.com/564x/27/ec/53/27ec5395bcfa4a9c2b6f2e032a1504bf.jpg"
+          alt="back"
+        />
       </div>
     </div>
   );
